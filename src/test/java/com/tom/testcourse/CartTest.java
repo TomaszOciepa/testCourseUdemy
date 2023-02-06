2b8b79891692d85ecc4f5ffa97f0d891cda1d4e1
tomszociepa <tom.ociepa@gmail.com>
2023-02-06 13:14:14 +0100

    @Disabled i @DisplayName

--- src/test/java/com/tom/testcourse/CartTest.java
+++ src/test/java/com/tom/testcourse/CartTest.java
@@ -1,21 +1,26 @@
 package com.tom.testcourse;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class CartTest {
 
+//    @Disabled
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 10 ms")
     void simulateLargeOrder(){
         //given
         Cart cart = new Cart();
 
         //When
         //Then
-        assertTimeout(Duration.ofMillis(10), cart::simulateLargeOrder);
+        assertTimeout(Duration.ofMillis(10 ), cart::simulateLargeOrder);
 
     }
 
